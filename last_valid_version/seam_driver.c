94313d9b171cdbcbdaebc850ee58ed3930212a5b
aleksandra.petrovic <aleksandrap9991.ap@gmail.com>
2022-09-30 15:32:16 +0200

    functions work without dma

--- last_valid_version/seam_driver.c
+++ last_valid_version/seam_driver.c
@@ -69,6 +69,7 @@ struct device_info
   unsigned long mem_start;
   unsigned long mem_end;
   void __iomem *base_addr;
+  int irq_num;
 };
 
 static struct device_info *seam    = NULL;
@@ -104,10 +105,10 @@ struct file_operations my_fops =
 // PROBE & REMOVE
 // ------------------------------------------
 
-int device_fsm = 0;
 
 static int seam_probe(struct platform_device *pdev)
 {
+  printk(KERN_INFO "[PROBE BEGINS");
   struct resource *r_mem;
   int rc = 0;
   r_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
@@ -115,11 +116,8 @@ static int seam_probe(struct platform_device *pdev)
 		printk(KERN_ALERT "invalid address\n");
 		return -ENODEV;
 	}
-  printk(KERN_INFO "Starting probing.\n");
+  printk(KERN_INFO "[PROBE BEGINS] Starting probing.\n");
 
-  switch (device_fsm)
-    {
-    case 0: // device seam
       seam = (struct device_info *) kmalloc(sizeof(struct device_info), GFP_KERNEL);
       if (!seam)
         {
@@ -143,15 +141,12 @@ static int seam_probe(struct platform_device *pdev)
           rc = -EIO;
           goto error2;
         }
-      ++device_fsm; ///samo ovo nema u vga_dma
 
-      //////DIFERENCES BEGIN
-      /*
 // Get irq num 
 	seam->irq_num = platform_get_irq(pdev, 0);
 	if(!seam->irq_num)
 	{
-		printk(KERN_ERR "seam_dma_probe: Could not get IRQ resource\n");
+		printk(KERN_ERR "seam_probe: Could not get IRQ resource\n");
 		rc = -ENODEV;
 		goto error2;
 	}
@@ -164,50 +159,36 @@ static int seam_probe(struct platform_device *pdev)
 	else {
 		printk(KERN_INFO "seam_dma_probe: Registered IRQ %d\n", seam->irq_num);
 	}
-
+/*
 	//INIT DMA 
 	dma_init(seam->base_addr);
 	dma_simple_write(tx_phy_buffer, MAX_PKT_LEN, seam->base_addr); // helper function, defined later
 */
-      ///////DIFERENCES END
       printk(KERN_INFO "[PROBE]: Finished probing seam.\n");
       return 0;
 
-      //error3:
-	      //iounmap(seam->base_addr);
+      error3:
+	      iounmap(seam->base_addr);
       error2:
         release_mem_region(seam->mem_start, seam->mem_end - seam->mem_start + 1);
       error1:
         return rc;
-      break;
-
-    default:
-      printk(KERN_INFO "[PROBE] Device FSM in illegal state. \n");
-      return -1;
-    }
-  printk(KERN_INFO "Succesfully probed driver\n");
+ 
+  printk(KERN_INFO "[PROBE ENDS] Succesfully probed driver\n");
   return 0;
 }
 
 static int seam_remove(struct platform_device *pdev)
 {
-  switch (device_fsm)
-    {
-    case 0: //device seam
-      //u32 reset = 0x00000004;
-      printk(KERN_ALERT "seam device platform driver removed\n");
-      iowrite32(0, seam->base_addr); /////iowrite32(reset, seam->base_addr); 
-      //free_irq(seam->irq_num, NULL);
-      iounmap(seam->base_addr);
-      release_mem_region(seam->mem_start, seam->mem_end - seam->mem_start + 1);
-      kfree(seam);
-      break;
-
-    default:
-      printk(KERN_INFO "[REMOVE] Device FSM in illegal state. \n");
-      return -1;
-    }
-  printk(KERN_INFO "Succesfully removed driver\n");
+  u32 reset = 0x00000004;
+  printk(KERN_ALERT "[REMOVE BEGINS] seam device platform driver removed\n");
+ // iowrite32(0, seam->base_addr); //0 or reset?
+  iowrite32(reset, seam->base_addr); 
+  free_irq(seam->irq_num, NULL);
+  iounmap(seam->base_addr);
+  release_mem_region(seam->mem_start, seam->mem_end - seam->mem_start + 1);
+  kfree(seam);
+  printk(KERN_INFO "[REMOVE ENDS] Succesfully removed driver\n");
   return 0;
 }
 
@@ -217,13 +198,13 @@ static int seam_remove(struct platform_device *pdev)
 
 static int seam_open(struct inode *pinode, struct file *pfile)
 {
-		printk(KERN_INFO "Succesfully opened file\n");
+		printk(KERN_INFO "[OPEN] Succesfully opened file\n");
 		return 0;
 }
 
 static int seam_close(struct inode *pinode, struct file *pfile)
 {
-		printk(KERN_INFO "Succesfully closed file\n");
+		printk(KERN_INFO "[CLOSE] Succesfully closed file\n");
 		return 0;
 }
 
@@ -231,8 +212,6 @@ static int seam_close(struct inode *pinode, struct file *pfile)
 // READ & WRITE
 // ------------------------------------------
 
-unsigned int log2wa=0, wa=0, log2ha=0, ha=0;
-unsigned int log2wb=0, wb=0, log2hb=0, hb=0;
 #define BUFF_SIZE 40
 int i = 0;
 int end_read = 0;
@@ -245,7 +224,7 @@ ssize_t seam_read(struct file *pfile, char __user *buffer, size_t length, loff_t
   //u32 val;
   int minor = MINOR(pfile->f_inode->i_rdev);
 
-  printk(KERN_INFO "Seam read entered \n"); 
+  printk(KERN_INFO "[READ BEGINS] Seam read entered \n"); 
   printk(KERN_INFO "i = %d, len = %ld, end_read = %d\n", i, len, end_read);
   if (end_read == 1)
     {
@@ -256,7 +235,7 @@ ssize_t seam_read(struct file *pfile, char __user *buffer, size_t length, loff_t
   switch (minor)
     {
     case 0: //device seam
-      printk(KERN_INFO "[READ] Succesfully read from seam device.\n");
+      printk(KERN_INFO "[READ ENDS] Succesfully read from seam device.\n");
       //signals here
       
       len = scnprintf(buf, BUFF_SIZE, "[READ] colsize = %d, rowsize = %d, start = %d, ready = %d\n", colsize, rowsize, start, ready);
@@ -285,11 +264,12 @@ ssize_t seam_write(struct file *pfile, const char __user *buffer, size_t length,
     {
     case 0: //device seam
       sscanf(buf, "%d", &colsize);
+      printk(KERN_INFO "[WRITE BEINGS] ");
       printk(KERN_INFO "%d, %d, %d\n", colsize, rowsize, start);
       iowrite32(colsize, seam->base_addr+COLSIZE);
       iowrite32(rowsize, seam->base_addr+ROWSIZE);
       iowrite32(start, seam->base_addr+START);
-      printk(KERN_INFO "[WRITE] Succesfully wrote into seam device.\n");
+      printk(KERN_INFO "[WRITE ENDS] Succesfully wrote into seam device.\n");
       break;
 
     default:
@@ -379,9 +359,9 @@ u32 dma_simple_write(dma_addr_t TxBufferPtr, u32 max_pkt_len, void __iomem *base
 static int __init seam_init(void)
 {
    printk(KERN_INFO "\n");
-   printk(KERN_INFO "SEAM driver starting insmod.\n");
+   printk(KERN_INFO "[INIT BEGIN] SEAM driver starting insmod.\n");
 
-   if (alloc_chrdev_region(&my_dev_id, 0, 1, "seam_region") < 0){  ////is 1 right number????
+   if (alloc_chrdev_region(&my_dev_id, 0, 1, "seam_region") < 0){  
       printk(KERN_ERR "failed to register char device\n");
       return -1;
    }
@@ -404,16 +384,17 @@ static int __init seam_init(void)
 	my_cdev->ops = &my_fops;
 	my_cdev->owner = THIS_MODULE;
 
-	if (cdev_add(my_cdev, my_dev_id, 1) == -1)  ///is 1 right number????
+	if (cdev_add(my_cdev, my_dev_id, 1) == -1) 
 	{
       printk(KERN_ERR "failed to add cdev\n");
       goto fail_2;
 	}
    printk(KERN_INFO "cdev added\n");
-   printk(KERN_INFO "Seam driver initialized.\n");
+   printk(KERN_INFO "[INIT 1 ENDS] Seam driver initialized.\n");
 
-///DIFERENCE STARTS HERE*************
+//DMA INIT 
 /*
+
   tx_vir_buffer = dma_alloc_coherent(NULL, MAX_PKT_LEN, &tx_phy_buffer, GFP_DMA | GFP_KERNEL); //GFP_KERNEL
   if(!tx_vir_buffer)
   {
@@ -427,17 +408,15 @@ static int __init seam_init(void)
   for (i = 0; i < MAX_PKT_LEN/4;i++)
   {
     tx_vir_buffer[i] = 0x00000000;
-  }
-  printk(KERN_INFO "DMA memory reset.\n");
-*/
-  ////DIFERENCE ENDS HERE************
+  }*/
+  printk(KERN_INFO "[INIT 2 ENDS] DMA memory reset.\n");
 
    return platform_driver_register(&my_driver);
 
-   // fail_3:
-     // cdev_del(my_cdev);
+    fail_3:
+      cdev_del(my_cdev);
     fail_2:
-     device_destroy(my_class, MKDEV(MAJOR(my_dev_id),0));  ////is 0 right number????
+     device_destroy(my_class, MKDEV(MAJOR(my_dev_id),0));  
     fail_1:
       class_destroy(my_class);
    fail_0:
@@ -447,14 +426,14 @@ static int __init seam_init(void)
 
 static void __exit seam_exit(void)
 {
-  printk(KERN_INFO "Seam driver starting rmmod.\n");
+  printk(KERN_INFO "[EXIT BEGINS] Seam driver starting rmmod.\n");
 	platform_driver_unregister(&my_driver);
 	cdev_del(my_cdev);
   device_destroy(my_class, MKDEV(MAJOR(my_dev_id),0));
   class_destroy(my_class);
   unregister_chrdev_region(my_dev_id,1);
   //dma_free_coherent(NULL, MAX_PKT_LEN, tx_vir_buffer, tx_phy_buffer);
-	printk(KERN_INFO "Seam driver exited.\n");
+	printk(KERN_INFO "[EXIT ENDS] Seam driver exited.\n");
 }
 
 module_init(seam_init);
